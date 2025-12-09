36ca9d36b233a2de7051ea465d68821fdf4636be
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2025-12-09 16:04:43 +0700

    feat: create platform stm32

---
+++ platforms/stm32/src/bin/spi_service_singleton.rs
@@ -0,0 +1,60 @@
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
+use embassy_executor::Spawner;
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    mode::Blocking,
+    spi::{Config, Spi},
+    time::Hertz,
+};
+use embassy_sync::blocking_mutex::{Mutex, raw::ThreadModeRawMutex};
+use embassy_time::Delay;
+use firmoto::{
+    allocator::init_allocator,
+    services::{spi_service::ConcreteSpiService, spi_service_traits::SpiServiceTrait},
+};
+use static_cell::StaticCell;
+
+type StaticBlockingSpiBusType = Mutex<ThreadModeRawMutex, RefCell<Spi<'static, Blocking>>>;
+
+type ConcreteSpiServiceType =
+    ConcreteSpiService<'static, ThreadModeRawMutex, Spi<'static, Blocking>, Output<'static>, Delay>;
+
+type MutexSpiService = Mutex<ThreadModeRawMutex, RefCell<Option<ConcreteSpiServiceType>>>;
+
+static SPI_SERVICE: MutexSpiService = Mutex::new(RefCell::new(None));
+
+static SPI_BUS: StaticCell<StaticBlockingSpiBusType> = StaticCell::new();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    init_allocator();
+
+    let p = embassy_stm32::init(Default::default());
+
+    let mut spi_config = Config::default();
+    spi_config.frequency = Hertz(1_000_000);
+
+    let cs = Output::new(p.PA2, Level::High, Speed::VeryHigh);
+    let spi = Spi::new_blocking(p.SPI3, p.PC10, p.PC12, p.PC11, spi_config);
+    let current_config = spi.get_current_config();
+    let static_bus = SPI_BUS.init(Mutex::new(RefCell::new(spi)));
+
+    let spi_device_with_config = SpiDeviceWithConfig::new(static_bus, cs, spi_config);
+    let service = ConcreteSpiService::new(spi_device_with_config, Delay, current_config);
+
+    SPI_SERVICE.lock(|cell| {
+        *cell.borrow_mut() = Some(service);
+    });
+
+    SPI_SERVICE.lock(|cell| {
+        if let Some(spi_dev) = cell.borrow_mut().as_mut() {
+            let _ = spi_dev.read_u8(8u8);
+        }
+    });
+
+    firmoto_stm32::exit()
+}
---
+++ platforms/stm32/src/bin/spi_service_singleton_spi_with_config.rs
@@ -0,0 +1,56 @@
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+use cortex_m_semihosting::hprintln;
+use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
+use embassy_executor::Spawner;
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    mode::Blocking,
+    spi::{Config, Spi},
+    time::Hertz,
+};
+use embassy_sync::blocking_mutex::{Mutex, raw::ThreadModeRawMutex};
+use embedded_hal::spi::SpiDevice as _;
+use firmoto::allocator::init_allocator;
+use static_cell::StaticCell;
+
+type SpiDevWithConfigType =
+    SpiDeviceWithConfig<'static, ThreadModeRawMutex, Spi<'static, Blocking>, Output<'static>>;
+
+type StaticBlockingSpiBusType = Mutex<ThreadModeRawMutex, RefCell<Spi<'static, Blocking>>>;
+
+type MutexSpiDevWithConfigType = Mutex<ThreadModeRawMutex, RefCell<Option<SpiDevWithConfigType>>>;
+
+static SPI_DEVICE_WITH_CONFIG: MutexSpiDevWithConfigType = Mutex::new(RefCell::new(None));
+
+static SPI_BUS: StaticCell<StaticBlockingSpiBusType> = StaticCell::new();
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    init_allocator();
+
+    hprintln!("will sync work?");
+    let p = embassy_stm32::init(Default::default());
+
+    let mut spi_config = Config::default();
+    spi_config.frequency = Hertz(1_000_000);
+
+    let cs = Output::new(p.PA2, Level::High, Speed::VeryHigh);
+    let spi = Spi::new_blocking(p.SPI3, p.PC10, p.PC12, p.PC11, spi_config);
+    let static_bus = SPI_BUS.init(Mutex::new(RefCell::new(spi)));
+
+    SPI_DEVICE_WITH_CONFIG.lock(|spi_dev_with_config| {
+        *spi_dev_with_config.borrow_mut() =
+            Some(SpiDeviceWithConfig::new(static_bus, cs, spi_config));
+    });
+
+    SPI_DEVICE_WITH_CONFIG.lock(|spi_dev_with_config| {
+        if let Some(spi_dev) = spi_dev_with_config.borrow_mut().as_mut() {
+            let _ = spi_dev.read(&mut [0u8]);
+        }
+    });
+
+    firmoto_stm32::exit()
+}
