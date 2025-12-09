b30db2b75b7294cf0d7cb9c5b9fc06e5f294b159
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2025-12-09 11:37:41 +0700

    feat: first implementation of SpiDeviceWithConfig Singleton compiled
    n

--- nostd_playground/src/bin/operation_dispatcher.rs
+++ nostd_playground/src/bin/operation_dispatcher.rs
@@ -1,12 +1,12 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
+use core::{cell::RefCell, ops::Deref};
 use cortex_m_semihosting::hprintln;
 use embassy_executor::Spawner;
-use embedded_hal::delay::DelayNs;
+use embedded_hal::{delay::DelayNs, spi::SpiDevice as EhSpiDevice};
 
-use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
+use embassy_embedded_hal::shared_bus::blocking::spi::{SpiDevice, SpiDeviceWithConfig};
 use embassy_stm32::{
     Peripherals,
     gpio::{Level, Output, Speed},
@@ -14,19 +14,51 @@ use embassy_stm32::{
     spi::{Config, Spi},
     time::Hertz,
 };
-use embassy_sync::blocking_mutex::{NoopMutex, raw::NoopRawMutex};
+use embassy_sync::blocking_mutex::{
+    Mutex, NoopMutex, ThreadModeMutex,
+    raw::{NoopRawMutex, ThreadModeRawMutex},
+};
 use embassy_time::{Delay, Timer};
-use nostd_playground::allocator::init_allocator;
-use nostd_playground::services::concrete_spi_service::ConcreteSpiService;
+use nostd_playground::{allocator::init_allocator, service_provider::SPI_DEVICE_ONLY};
+use nostd_playground::{
+    service_provider::Stm32SpiService, services::concrete_spi_service::ConcreteSpiService,
+};
 use static_cell::StaticCell;
 
-static SPI_BUS: StaticCell<NoopMutex<RefCell<Spi<'_, Blocking>>>> = StaticCell::new();
+type Sdwc =
+    SpiDeviceWithConfig<'static, ThreadModeRawMutex, Spi<'static, Blocking>, Output<'static>>;
+
+static SPI_DEVICE_WITH_CONFIG: Mutex<ThreadModeRawMutex, RefCell<Option<Sdwc>>> =
+    Mutex::new(RefCell::new(None));
+type SpiBusTy<'a> = Mutex<ThreadModeRawMutex, RefCell<Spi<'a, Blocking>>>;
+static SPI_BUS_SC: StaticCell<SpiBusTy<'_>> = StaticCell::new();
 
 // NOTE:
 // reference
 //  - https://docs.embassy.dev/embassy-embedded-hal/git/default/shared_bus/blocking/spi/index.html
 //  - https://github.com/embassy-rs/embassy/blob/main/examples/stm32f4/src/bin/spi.rs
 //
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    init_allocator();
+
+    hprintln!("will sync work?");
+    let p = embassy_stm32::init(Default::default());
+
+    let mut spi_config = Config::default();
+    spi_config.frequency = Hertz(1_000_000);
+
+    let cs = Output::new(p.PA2, Level::High, Speed::VeryHigh);
+    let spi = Spi::new_blocking(p.SPI3, p.PC10, p.PC12, p.PC11, spi_config);
+    let static_bus = SPI_BUS_SC.init(Mutex::new(RefCell::new(spi)));
+
+    SPI_DEVICE_WITH_CONFIG.lock(|spi_dev_with_config| {
+        *spi_dev_with_config.borrow_mut() =
+            Some(SpiDeviceWithConfig::new(static_bus, cs, spi_config));
+    });
+    nostd_playground::exit()
+}
 
 // #[cortex_m_rt::entry]
 // fn main() -> ! {
@@ -50,29 +82,3 @@ static SPI_BUS: StaticCell<NoopMutex<RefCell<Spi<'_, Blocking>>>> = StaticCell::
 //
 //     nostd_playground::exit()
 // }
-
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    init_allocator();
-
-    hprintln!("will sync work?");
-    let p = embassy_stm32::init(Default::default());
-
-    let mut spi_config = Config::default();
-    spi_config.frequency = Hertz(1_000_000);
-
-    let cs = Output::new(p.PA2, Level::High, Speed::VeryHigh);
-    let spi = Spi::new_blocking(p.SPI3, p.PC10, p.PC12, p.PC11, spi_config);
-    let current_config = spi.get_current_config();
-    let spi_bus = NoopMutex::new(RefCell::new(spi));
-    let spi_bus = SPI_BUS.init(spi_bus);
-
-    let spi_device_with_config = SpiDeviceWithConfig::new(spi_bus, cs, spi_config);
-    // SpiService::new(spi_device_with_config, Delay, current_config);
-
-    // let my_pet = get_singleton();
-    //
-    // hprintln!("my pet is {}", my_pet.name);
-
-    nostd_playground::exit()
-}
--- nostd_playground/src/service_provider.rs
+++ nostd_playground/src/service_provider.rs
@@ -1,4 +1,7 @@
+use crate::services::ServiceTrait;
 use crate::services::concrete_spi_service::ConcreteSpiService;
+use crate::services::spi_service_traits::SpiServiceTrait;
+use alloc::boxed::Box;
 use core::cell::RefCell;
 use embassy_stm32::Peripherals;
 use embassy_stm32::{gpio::Output, mode::Blocking, spi::Spi};
@@ -11,30 +14,63 @@ pub type Stm32SpiService<'a> =
     ConcreteSpiService<'a, NoopRawMutex, Spi<'a, Blocking>, Output<'a>, Delay>;
 // static CONCRETE_SPI_SERVICE: StaticCell<Stm32SpiService<'_>> = StaticCell::new();
 
-struct SpiMo;
-impl ErrorType for SpiMo {
-    type Error = ErrorKind;
-}
-impl SpiDevice for SpiMo {
-    fn transaction(
-        &mut self,
-        _operations: &mut [embedded_hal::spi::Operation<'_, u8>],
-    ) -> Result<(), Self::Error> {
-        Ok(())
+struct Stm32ConcreteSpiService;
+
+impl ServiceTrait for Stm32ConcreteSpiService {
+    fn init(&mut self) {
+        todo!()
+    }
+
+    fn deinit(&mut self) {
+        todo!()
     }
 }
 
-static PERIPHERAL: Mutex<ThreadModeRawMutex, RefCell<Option<Peripherals>>> =
+impl SpiServiceTrait for Stm32ConcreteSpiService {
+    fn write_u8(
+        &mut self,
+        address: u8,
+        data: u8,
+    ) -> Result<(), crate::services::spi_service_traits::SpiServiceError> {
+        todo!()
+    }
+
+    fn read_u16(&mut self, address: u8) -> u16 {
+        todo!()
+    }
+
+    fn read_u8(&mut self, address: u8) -> u8 {
+        todo!()
+    }
+
+    fn read_n<const N: usize>(&mut self, address: u8, buffer: &mut [u8; N]) {
+        todo!()
+    }
+}
+
+type EEHSpiDevice = embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice<
+    'static,
+    ThreadModeRawMutex,
+    Spi<'static, Blocking>,
+    Output<'static>,
+>;
+
+pub static SPI_DEVICE_ONLY: Mutex<ThreadModeRawMutex, RefCell<Option<EEHSpiDevice>>> =
     Mutex::new(RefCell::new(None));
 
-static SPI: Mutex<ThreadModeRawMutex, RefCell<Option<SpiMo>>> =
-    Mutex::new(RefCell::new(Some(SpiMo)));
+// static SPI_DEVICE: Mutex<ThreadModeRawMutex, RefCell<Option>> = Mutex::new(RefCell::new(None));
+
+// static GENERIC_SPI_SINGLETON: Mutex<ThreadModeRawMutex, RefCell<Option<dyn ServiceTrait>>> =
+//     Mutex::new(RefCell::new(None));
+
+static SPI_SINGLETON: Mutex<ThreadModeRawMutex, RefCell<Option<Stm32ConcreteSpiService>>> =
+    Mutex::new(RefCell::new(None));
 
 fn try_mutex_spi() {
-    SPI.lock(|cell| {
+    SPI_SINGLETON.lock(|cell| {
         let mut opt = cell.borrow_mut();
         if let Some(dev) = opt.as_mut() {
-            let _ = dev.transaction(&mut [Operation::Read(&mut [1u8])]);
+            let _ = dev.read_u8(8u8);
         }
     })
 }
